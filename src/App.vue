b6c1489278cfeea478be292c644d11ad161b6524
jinho.cho <jinho.cho@magnumbridge.co.kr>
2023-10-12 22:43:41 +0900

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   
   <div class="black-bg" v-if="모달창열렸니==true">
     <div class="white-bg">
-      <h4>상세페이지임</h4>
+      <h4>webtob는 과연...알아서 바뀌나..?</h4>
       <p>상세페이지 내용임</p>
       <button @click="모달창열렸니=false">닫기</button>
     </div>
